01f3967d68a998ee89ce0f4a70a91c8606b7bd3f
MarcKlesiewicz <marc247hedu.easj.dk>
2020-02-18 15:11:03 +0100

    milepæl 2 done

--- app/src/main/java/com/example/android/milepeale/MainActivity.java
+++ app/src/main/java/com/example/android/milepeale/MainActivity.java
@@ -3,13 +3,41 @@ package com.example.android.milepeale;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText insertText;
+    TextView showText;
+    Button clickMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insertText = (EditText)findViewById(R.id.insertText);
+        showText = (TextView)findViewById(R.id.showText);
+        clickMe = (Button)findViewById(R.id.clickMe);
+
+        clickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showText.setText(insertText.getText().toString());
+
+
+            }
+        });
+
+
+
+
+
+
     }
 }
